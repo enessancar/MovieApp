f35c9a0a36bcaa5eea7e87bd0a7ac5eb8b21a587
enessancar <enes57751907@gmail.com>
2023-10-04 18:27:16 +0300

    I created flowlayout for collection view in uihelper file

--- MovieApp/Managers/NetworkManager.swift
+++ MovieApp/Managers/NetworkManager.swift
@@ -5,4 +5,18 @@
 //  Created by Enes Sancar on 4.10.2023.
 //
 
-import Foundation
+import UIKit
+
+final class NetworkingManager {
+    static let shared = NetworkingManager()
+    private init() {}
+    
+    let cache: NSCache<NSString, UIImage> = {
+        let cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 100
+        cache.totalCostLimit = 1024 * 1024 * 250
+        return cache
+    }()
+    
+    
+}
--- MovieApp/Utils/UIhelper.swift
+++ MovieApp/Utils/UIhelper.swift
@@ -8,5 +8,48 @@
 import UIKit
 
 enum UIhelper {
+    static func createFlowLayout() -> UICollectionViewFlowLayout {
+        let flowlayout = UICollectionViewFlowLayout()
+        
+        let width = UIScreen.main.bounds.width
+        let itemWidth = width / 2.15
+        
+        flowlayout.itemSize = .init(width: itemWidth, height: itemWidth * 1.5)
+        flowlayout.scrollDirection = .horizontal
+        return flowlayout
+    }
     
+    static func createCastFlowLayout() -> UICollectionViewFlowLayout {
+        let flowlayout = UICollectionViewFlowLayout()
+        
+        flowlayout.itemSize = .init(width: 160, height: 290)
+        flowlayout.scrollDirection = .horizontal
+        return flowlayout
+    }
+    
+    static func createExploreFlowLayout() -> UICollectionViewFlowLayout {
+        let flowlayout = UICollectionViewFlowLayout()
+        
+        let width = UIScreen.main.bounds.width
+        let itemWidth = width / 2.15
+        
+        flowlayout.sectionInset = .init(top: 10, left: 10, bottom: 30, right: 10)
+        flowlayout.itemSize = .init(width: itemWidth, height: itemWidth * 1.5)
+        flowlayout.scrollDirection = .vertical
+        
+        return flowlayout
+    }
+    
+    static func createWatchlistFlowLayout() -> UICollectionViewFlowLayout {
+        let flowlayout = UICollectionViewFlowLayout()
+        
+        let width = UIScreen.main.bounds.width
+        let itemWidth = width / 3.3
+        
+        flowlayout.sectionInset = .init(top: 10, left: 10, bottom: 30, right: 10)
+        flowlayout.itemSize = .init(width: itemWidth, height: itemWidth * 1.5)
+        flowlayout.scrollDirection = .vertical
+        
+        return flowlayout
+    }
 }
