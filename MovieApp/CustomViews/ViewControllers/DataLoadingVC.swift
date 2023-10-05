365276cbe2e6e9f33d34609c13fcd54f4ae513fa
enessancar <enes57751907@gmail.com>
2023-10-05 21:45:32 +0300

    I created data loading vc

--- MovieApp/CustomViews/ViewControllers/DataLoadingVC.swift
+++ MovieApp/CustomViews/ViewControllers/DataLoadingVC.swift
@@ -5,4 +5,38 @@
 //  Created by Enes Sancar on 5.10.2023.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class DataLoadingVC: UIViewController {
+    
+    private var containerView: UIView!
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            self.containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+}
+
