7bb53b206e6cbd534ad55955312a3e18e62f6fcb
enessancar <enes57751907@gmail.com>
2023-10-05 15:03:59 +0300

    I created cast stack view

--- MovieApp/CustomViews/Views/CastView.swift
+++ MovieApp/CustomViews/Views/CastView.swift
@@ -5,4 +5,56 @@
 //  Created by Enes Sancar on 5.10.2023.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class CastView: UIStackView {
+    
+    private var superContainerView: UIStackView!
+    private let titleLabel = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    private var collectionView: UICollectionView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    
+    init(superContainerView: UIStackView) {
+        super.init(frame: .zero)
+        self.superContainerView = superContainerView
+        
+        configureCastView()
+        configureTitleLabel()
+        configureCollectionView()
+    }
+    
+    private func configureCastView() {
+        superContainerView.addArrangedSubview(self)
+        axis = .vertical
+        distribution = .fill
+    }
+    
+    private func configureTitleLabel() {
+        addArrangedSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.height.equalTo(30)
+        }
+        titleLabel.text = "Top Billed Cast"
+    }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UIhelper.createCastFlowLayout())
+        addArrangedSubview(collectionView)
+        
+        collectionView.backgroundColor = .systemBackground
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(TopBilledCell.self, forCellWithReuseIdentifier: TopBilledCell.identifier)
+        
+        collectionView.snp.makeConstraints { make in
+            make.height.equalTo(300)
+        }
+    }
+}
